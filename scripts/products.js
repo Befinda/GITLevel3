f09391fde29a36cd73cfee9cf483e3932cadb4db
Margarita Mareeva <beffinda@gmail.com>
2022-11-27 19:10:15 +0300

    база товаров

---
+++ scripts/products.js
@@ -0,0 +1,60 @@
+const products = [
+    new ProductDTO(
+        0,
+        "product1.jpg",
+        "Product 1",
+        "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        10,
+    ),
+    new ProductDTO(
+        1,
+        "product7.jpg",
+        "Product 7",
+        "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        20,
+    ),
+    new ProductDTO(
+        2,
+        "product3.jpg",
+        "Product 3",
+        "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        30,
+    ),
+    new ProductDTO(
+        3,
+        "product4.jpg",
+        "Product 4",
+        "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        40,
+    ), new ProductDTO(
+        4,
+        "product8.jpg",
+        "Product 8",
+        "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        50,
+    ), new ProductDTO(
+        5,
+        "product9.jpg",
+        "Product 9",
+        "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        60,
+    ), new ProductDTO(
+        6,
+        "product10.jpg",
+        "Product 10",
+        "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        70,
+    ), new ProductDTO(
+        7,
+        "product11.jpg",
+        "Product 11",
+        "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        80,
+    ), new ProductDTO(
+        8,
+        "product12.jpg",
+        "Product 12",
+        "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        90,
+    ),
+]
